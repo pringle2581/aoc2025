a5034023a63110c2324f0d63683223ba525fa214
pringle2581 <kmjohnstonjr@gmail.com>
2025-12-01 17:28:51 -0500

    day 01

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using aoc2025.solutions;
+
+bool complete = false;
+string input = "../../../input/";
+string[] results = [];
+while (!complete)
+{
+    Console.WriteLine("Select a day");
+    string? daystring = Console.ReadLine();
+    int day = int.TryParse(daystring, out int i) ? i : 0;
+
+    switch (day)
+    {
+        case 1:
+            results = Day01.Solve(File.ReadAllLines(input + "01"));
+            complete = true;
+            break;
+        default:
+            Console.WriteLine("Invalid day, try again\n");
+            break;
+    }
+}
+
+Console.WriteLine($"Part 1: {results[0]}\nPart 2: {results[1]}");
---
+++ solutions/Day01.cs
@@ -0,0 +1,52 @@
+﻿namespace aoc2025.solutions
+{
+    internal class Day01
+    {
+        static public string[] Solve(string[] input)
+        {
+            int part1 = 0, part2 = 0;
+            List<int> rotations = Parse(input);
+            Solve(rotations);
+            return [part1.ToString(), part2.ToString()];
+
+            List<int> Parse(string[] input)
+            {
+                List<int> list = [];
+                foreach (string line in input)
+                {
+                    int num = int.Parse(line[1..]);
+                    if (line[..1] == "L")
+                    {
+                        num *= -1;
+                    }
+                    list.Add(num);
+                }
+                return list;
+            }
+
+            void Solve(List<int> steps)
+            {
+                int dial = 50;
+                foreach (var step in steps)
+                {
+                    dial = Turn(dial, step);
+                    if (dial == 0) {
+                        part1++;
+                    }
+                }
+            }
+
+            int Turn(int dial, int step)
+            {
+                for (int i = 0; i < Math.Abs(step); i++)
+                {
+                    dial = dial + Math.Sign(step);
+                    if (dial == -1) { dial = 99; }
+                    if (dial == 100) { dial = 0; }
+                    if (dial == 0) { part2++; }
+                }
+                return dial;
+            }
+        }
+    }
+}
