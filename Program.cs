468fce6aad2ca1979ed9dc6d27d4802559745320
pringle2581 <kmjohnstonjr@gmail.com>
2025-12-02 12:52:26 -0500

    day 02

--- Program.cs
+++ Program.cs
@@ -1,23 +1,58 @@
 ﻿using aoc2025.solutions;
 
-bool complete = false;
 string input = "../../../input/";
 string[] results = [];
+bool complete = false;
 while (!complete)
 {
     Console.WriteLine("Select a day");
     string? daystring = Console.ReadLine();
     int day = int.TryParse(daystring, out int i) ? i : 0;
-
     switch (day)
     {
         case 1:
             results = Day01.Solve(File.ReadAllLines(input + "01"));
-            complete = true;
             break;
+        case 2:
+            results = Day02.Solve(File.ReadAllLines(input + "02"));
+            break;
+        //case 3:
+        //    results = Day03.Solve(File.ReadAllLines(input + "03"));
+        //    break;
+        //case 4:
+        //    results = Day04.Solve(File.ReadAllLines(input + "04"));
+        //    break;
+        //case 5:
+        //    results = Day05.Solve(File.ReadAllLines(input + "05"));
+        //    break;
+        //case 6:
+        //    results = Day06.Solve(File.ReadAllLines(input + "06"));
+        //    break;
+        //case 7:
+        //    results = Day07.Solve(File.ReadAllLines(input + "07"));
+        //    break;
+        //case 8:
+        //    results = Day08.Solve(File.ReadAllLines(input + "08"));
+        //    break;
+        //case 9:
+        //    results = Day09.Solve(File.ReadAllLines(input + "09"));
+        //    break;
+        //case 10:
+        //    results = Day10.Solve(File.ReadAllLines(input + "10"));
+        //    break;
+        //case 11:
+        //    results = Day11.Solve(File.ReadAllLines(input + "11"));
+        //    break;
+        //case 12:
+        //    results = Day12.Solve(File.ReadAllLines(input + "12"));
+        //    break;
         default:
             Console.WriteLine("Invalid day, try again\n");
             break;
+    }
+    if (results.Length == 2)
+    {
+        complete = true;
     }
 }
 
---
+++ solutions/Day02.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc2025.solutions
+{
+    internal class Day02
+    {
+        static public string[] Solve(string[] input)
+        {
+            long part1 = 0, part2 = 0;
+            List<(long, long)> ranges = Parse(input);
+            Solve(ranges);
+            return [part1.ToString(), part2.ToString()];
+
+            List<(long, long)> Parse(string[] input)
+            {
+                List<(long, long)> list = [];
+                foreach (string line in input[0].Split(","))
+                {
+                    string[] range = line.Split("-");
+                    list.Add((long.Parse(range[0]), long.Parse(range[1])));
+                }
+                return list;
+            }
+
+            void Solve(List<(long start, long end)> ranges)
+            {
+                foreach (var (start, end) in ranges)
+                {
+                    for (long id = start; id <= end; id++)
+                    {
+                        if (!Validate(id))
+                        {
+                            part2 += id;
+                        }
+                    }
+                }
+            }
+
+            bool Validate(long id)
+            {
+                bool valid = true;
+                string idstr = id.ToString();
+                for (int slicelength = 1; slicelength <= idstr.Length / 2; slicelength++)
+                {
+                    if (idstr.Length % slicelength == 0)
+                    {
+                        HashSet<string> slices = [];
+                        for (int slicer = 0; slicer < idstr.Length; slicer += slicelength)
+                        {
+                            slices.Add(idstr[slicer..(slicer + slicelength)]);
+                        }
+                        if (slices.Count == 1)
+                        {
+                            if (idstr.Length / slicelength == 2)
+                            {
+                                part1 += id;
+                            }
+                            valid = false;
+                        }
+                    }
+                }
+                return valid;
+            }
+        }
+    }
+}
