b8121ee31689d31d5db195087bcc785d77c3fb6d
pringle2581 <kmjohnstonjr@gmail.com>
2025-12-09 21:20:27 -0500

    day 09

--- solutions/Day09.cs
+++ solutions/Day09.cs
@@ -25,14 +25,14 @@
 
                 for (int i = 1; i < reds.Count; i++)
                 {
-                    greens.Add(Green(reds[i - 1], reds[i]));
+                    greens.Add(Ranges(reds[i - 1], reds[i]));
                 }
-                greens.Add(Green(reds[0], reds[^1]));
+                greens.Add(Ranges(reds[0], reds[^1]));
 
                 foreach (var combo in GetCombinations(reds.Count, 2))
                 {
-                    (long,long)[] cornerpair = [reds[combo[0] - 1], reds[combo[1] - 1]];
-                    rectangles.Add(((cornerpair), Area(cornerpair)));
+                    (long,long)[] corners = [reds[combo[0] - 1], reds[combo[1] - 1]];
+                    rectangles.Add(((corners), Area(corners)));
                 }
                 rectangles.Sort((a, b) => b.area.CompareTo(a.area));
             }
@@ -52,7 +52,7 @@
                 return (part1, part2);
             }
 
-            static (long, long, long, long) Green((long x, long y) t1, (long x, long y) t2)
+            static (long xmin, long xmax, long ymin, long ymax) Ranges((long x, long y) t1, (long x, long y) t2)
             {
                 long xmin = Math.Min(t1.x, t2.x);
                 long xmax = Math.Max(t1.x, t2.x);
@@ -63,13 +63,10 @@
 
             bool TestSquare((long x, long y)[] c)
             {
-                foreach (var greenline in greens)
+                foreach (var green in greens)
                 {
-                    long xmin = Math.Min(c[0].x, c[1].x);
-                    long xmax = Math.Max(c[0].x, c[1].x);
-                    long ymin = Math.Min(c[0].y, c[1].y);
-                    long ymax = Math.Max(c[0].y, c[1].y);
-                    if (xmin < greenline.xmax && xmax > greenline.xmin && ymin < greenline.ymax && ymax > greenline.ymin)
+                    var (xmin, xmax, ymin, ymax) = Ranges(c[0], c[1]);
+                    if (xmin < green.xmax && xmax > green.xmin && ymin < green.ymax && ymax > green.ymin)
                     {
                         return false;
                     }
